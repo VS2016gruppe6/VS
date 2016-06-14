5589738f37d20d83c218954001633fc94e175a09
Ji <yuan.ji@fau.de>
2016-06-14 16:59:17 +0200

    better format

--- Aufgabe3/src/vsue/rpc/VSBuggyObjectConnection.java
+++ Aufgabe3/src/vsue/rpc/VSBuggyObjectConnection.java
@@ -5,56 +5,57 @@ import java.io.Serializable;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class VSBuggyObjectConnection extends VSObjectConnection {
+public class VSBuggyObjectConnection extends VSObjectConnection { // simuliation
+																	// kommnication
+																	// delay.
 	private double i = 0.0;
-	private int time = 400;
+	private int time = 400;   //ms
 	VSConnection connect;
+
 	public VSBuggyObjectConnection(VSConnection connect) {
 		super(connect);
 		this.connect = connect;
-		
 	}
-	//override
-	public void sendObject(Serializable object) throws IOException{
-		final Serializable _object = object;
-		i = Math.random();
-		System.out.println("i = "+i);
-	if(i <= 0.25){
-		System.out.println("send object this time normal");
-		super.sendObject(_object);
-	}
-	else if(i > 0.25 && i <= 0.5){
-		System.out.println("send object after "+time+" thread sleep");
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				VSObjectConnection vsconnect = new VSObjectConnection(connect);
-				try {
-					vsconnect.sendObject(_object);
-					this.cancel();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+
+	// override
+	public void sendObject(Serializable object) throws IOException {
+		final Serializable _object = object;  
+		i = Math.random();   //[0.0,1.0]
+		System.out.println("i = " + i);
+		if (i <= 0.25) {
+			System.out.println("send object this time normal");
+			super.sendObject(_object);
+		} else if (i > 0.25 && i <= 0.5) {
+			System.out.println("send object after " + time + " thread sleep");
+			Timer timer = new Timer();   //??.....put timer outside
+			timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					VSObjectConnection vsconnect = new VSObjectConnection(connect);
+					try {
+						vsconnect.sendObject(_object);
+						this.cancel();     //cancle timer
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
-			}
-		}, time);
-		
+			}, time);
+
+		} else if (i > 0.5 && i <= 0.75) {
+			System.out.println("don't send object this time");
+			return;
+		} else if (i > 0.75 && i <= 1.0) {
+			System.out.println("don't send object after " + time
+					+ "thread sleep");
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					return;
+				}
+			}, time);
+
+		}
 	}
-	else if (i > 0.5 && i <= 0.75){
-		System.out.println("don't send object this time");
-		return;
-	}
-	else if (i > 0.75 && i <= 1.0){
-		System.out.println("don't send object after "+time+"thread sleep");
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				return;
-			}
-		}, time);
-		
-	}
-}
 }
--- Aufgabe3/src/vsue/rpc/VSInvocationHandler.java
+++ Aufgabe3/src/vsue/rpc/VSInvocationHandler.java
@@ -9,12 +9,11 @@ import java.net.SocketTimeoutException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-
 public class VSInvocationHandler implements InvocationHandler, Serializable {
 
-	private VSRemoteReference remote; /// remoteReference
+	private VSRemoteReference remote; // / remoteReference
 
-	static final private int maxNr = 10;
+	static final private int maxNr = 10; // send Nr.
 	private int requestId = 0;
 	private int socketTimeout = 500;
 
@@ -29,7 +28,7 @@ public class VSInvocationHandler implements InvocationHandler, Serializable {
 	// return false;
 	// }
 	//
-	//// same request exists,return requestID,otherwise return -1
+	// // same request exists,return requestID,otherwise return -1
 	// public int getRuestID(Object proxy, Method method, Object[] args){
 	// requests tempRequest=new requests(this,method,args,0);
 	//
@@ -89,92 +88,113 @@ public class VSInvocationHandler implements InvocationHandler, Serializable {
 
 	// fuell alle lokalen aufrufe am Proxy
 	@SuppressWarnings("resource")
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
 		// every time requestId add 1
 		requestId++;
+		// init
 		Socket socket = null;
-		socket = new Socket(remote.getHost(), remote.getPort());
-		VSBuggyObjectConnection connect = new VSBuggyObjectConnection(new VSConnection(socket));;
+		socket = new Socket(remote.getHost(), remote.getPort()); // socket init
+		VSBuggyObjectConnection connect = new VSBuggyObjectConnection(
+				new VSConnection(socket)); // buggy delay
 		VSRevMsg revMsg = null;
 		VSSenMsg senMsg = null;
+
+		// send all object
 		Object[] toSend;
 		if (args == null) {
 			toSend = new Object[0];
 		} else {
-			toSend = args.clone();
+			toSend = args.clone(); // why clone?
 			for (int i = 0; i < toSend.length; i++) {
 				if (toSend[i] instanceof Remote) {
-					Remote stub = VSRemoteObjectManager.getInstance().getproxy((Remote) toSend[i]);
+					Remote stub = VSRemoteObjectManager.getInstance().getproxy(
+							(Remote) toSend[i]);
 					toSend[i] = stub;
 				}
 			}
 		}
-		A:for (int i = 0; i < maxNr; i++) {
+
+		//send process
+		A: for (int i = 0; i < maxNr; i++) { // continue --A
 			try {
-				senMsg = new VSSenMsg(remote.getObjectID(), method.toGenericString(), toSend, requestId, i);
+				senMsg = new VSSenMsg(remote.getObjectID(),
+						method.toGenericString(), toSend, requestId, i);
 				connect.sendObject(senMsg);
-				//System.out.println("send message ");
+				// System.out.println("send message ");
 			} catch (Exception e) {
 				e.printStackTrace();
-				System.out.println("unable to send proxy in invocationhandler involke!");
+				System.out
+						.println("unable to send proxy in invocationhandler involke!");
 			}
-			int runouttime = 0;
+
+			int runouttime = 0; // ? reset to 0?
 			long recvbegin = 0;
 			long recvend = 0;
-			for(int resttime = socketTimeout - runouttime; resttime > 0;) {
+
+			for (int resttime = socketTimeout - runouttime; resttime > 0;) { // logic?
 				try {
 					// antwort empfangen
-						socket.setSoTimeout(resttime);
-						recvbegin =  System.currentTimeMillis();
-//						System.out.println("recvbegin = " + recvbegin);
-						revMsg = (VSRevMsg) connect.receiveObject();
-						recvend = System.currentTimeMillis();
-//						System.out.println("recvend = " + recvend);
-						runouttime = (int)(recvend - recvbegin);
-						System.out.println("runouttime = " + runouttime);
-						resttime -= runouttime;
-						System.out.println("resttime is " + resttime);
-						if (resttime <= 0) {
-							continue A;
-						}
-						// // latest antwort comes,return
-						if (!(revMsg.getRequestID() == requestId && revMsg.getSequenzNr() == i)) {			
-							continue;
-						}
-						System.out.println("get right answer for requestID "+requestId+" at " + i+" SequenzNr");
-						break;
-				}catch (SocketTimeoutException e) {
+					socket.setSoTimeout(resttime);
+					recvbegin = System.currentTimeMillis(); // get now time
+					// System.out.println("recvbegin = " + recvbegin);
+					revMsg = (VSRevMsg) connect.receiveObject();
+					recvend = System.currentTimeMillis();
+					
+					// System.out.println("recvend = " + recvend);
+					runouttime = (int) (recvend - recvbegin);
+					System.out.println("runouttime = " + runouttime);
+					resttime -= runouttime;
+					System.out.println("resttime is " + resttime);
+					
+					if (resttime <= 0) {   //timeout
+						continue A;
+					}
+					// // latest antwort comes,return
+					if (!(revMsg.getRequestID() == requestId && revMsg
+							.getSequenzNr() == i)) {
+						continue;
+					}
+					System.out.println("get right answer for requestID "
+							+ requestId + " at " + i + " SequenzNr");
+					break;
+				} catch (SocketTimeoutException e) {
 					recvend = System.currentTimeMillis();
 					System.out.println("sockettimeout at " + recvend);
-					runouttime = (int)(recvend - recvbegin);
+					runouttime = (int) (recvend - recvbegin);
 					System.out.println("runouttime = " + runouttime);
 					resttime -= runouttime;
 					System.out.println("resttime is " + resttime);
 					if (resttime <= 0) {
 						continue A;
 					}
-					System.out.println("request" + " " + requestId + " " + "timeout" + " " + i);
+					System.out.println("request" + " " + requestId + " "
+							+ "timeout" + " " + i);
 					continue;
-				}catch (Exception e) {
+				} catch (Exception e) {
 					System.out.println("something wrong" + i);
 					System.out.println(e.getMessage());
 				}
 			}
 			break;
 		}
-		if(revMsg == null){
-			 throw new RemoteException("unable to get response ");
+		
+		
+		
+		if (revMsg == null) {
+			throw new RemoteException("unable to get response ");
 		}
-			//System.out.println("+++++++get right response+++++++");
+		// System.out.println("+++++++get right response+++++++");
 		Throwable exc = revMsg.getFehler();
 		if (exc != null) {
 			Type[] allowedexceptiontypescollection = method.getGenericExceptionTypes();
 			for (Type singleallowedexception : allowedexceptiontypescollection) {
-				if (((Class<?>) singleallowedexception).isAssignableFrom(exc.getClass())) {
+				if (((Class<?>) singleallowedexception).isAssignableFrom(exc.getClass())) {  //?
 					throw exc;
 				}
 			}
 		}
-		return revMsg.getResult();// not latest antwort,receive until timeout and next request
-	} 
+		return revMsg.getResult();// not latest antwort,receive until timeout
+									// and next request
+	}
 }

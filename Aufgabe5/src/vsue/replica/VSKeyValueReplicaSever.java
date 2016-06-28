e6fa99bb11a8712d5d708ba761230393b5e7f5e7
Ji <yuan.ji@fau.de>
2016-06-28 11:56:20 +0200

    JGrouops connect worked!

--- Aufgabe5/src/vsue/replica/VSKeyValueReplicaSever.java
+++ Aufgabe5/src/vsue/replica/VSKeyValueReplicaSever.java
@@ -4,18 +4,33 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+import org.jgroups.*;
+
 public class VSKeyValueReplicaSever {
-	 
+
 	public static void main(String[] args) throws Exception {
 
 		VSKeyValueRequestHandler Replica = new VSKeyValueReplica();
 
-		VSKeyValueRequestHandler ReplicaExport = (VSKeyValueRequestHandler) UnicastRemoteObject.exportObject(Replica, 0);
-				
-		Registry registry =LocateRegistry.createRegistry(12345);
-		
+		VSKeyValueRequestHandler ReplicaExport = (VSKeyValueRequestHandler) UnicastRemoteObject
+				.exportObject(Replica, 0);
+
+		Registry registry = LocateRegistry.createRegistry(12345);
+
 		registry.bind("service", ReplicaExport);
-		
+
+		//for JGropus
+		JChannel channel = new JChannel();
+		channel.setReceiver(new ReceiverAdapter() {
+			public void receive(Message msg) {
+				System.out.println("received msg from " + msg.getSrc() + ": "
+						+ msg.getObject());
+			}
+		});
+		channel.connect("gruppe6");
+		channel.send(new Message(null, "hello world"));
+		//channel.close();
+
 		System.out.println("Replica Service Start!");
 	}
 

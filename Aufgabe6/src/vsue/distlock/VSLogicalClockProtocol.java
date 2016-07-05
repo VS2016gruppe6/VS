a01e4fa8f43229fd2586afa4fdc5a2466f3ffffd
Zhouzhuo He <lu58pila@stud.informatik.uni-erlangen.de>
2016-07-05 17:11:32 +0200

    added:clock header

--- Aufgabe6/src/vsue/distlock/VSLogicalClockProtocol.java
+++ Aufgabe6/src/vsue/distlock/VSLogicalClockProtocol.java
@@ -9,27 +9,48 @@ import org.jgroups.Header;
 import org.jgroups.Message;
 import org.jgroups.conf.ClassConfigurator;
 import org.jgroups.stack.Protocol;
+import org.jgroups.util.Util;
+
+import vsue.distlock.FIFOUnicast.SimulatedUnicastProtocolHeader;
 
 public class VSLogicalClockProtocol extends Protocol {
 	
+	public int ProtocolCounter=0;
+	
 	public static class ClockHeader extends Header {
 		public static final short header_id = 1501;
-
+		public int HeaderCounter;
 		// Headers need a default constructor for instantiation via reflection.
 		public ClockHeader(/* Don't add parameters here! */) { }
 		
+		public ClockHeader(int Counter){
+			HeaderCounter = Counter;
+		}
+		
 		@Override
 		public void writeTo(DataOutput out) throws IOException {
-			// XXX IMPLEMENT ME XXX
+			try {
+				Util.writeObject(this, out);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
 		}
 		
 		@Override
 		public void readFrom(DataInput in) throws IOException {
-			// XXX IMPLEMENT ME XXX
+			ClockHeader Input = null;
+			try {
+				Input = (ClockHeader) Util.readObject(in);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			HeaderCounter = Input.HeaderCounter;	
 		}
 		
 		@Override
-		public int size() {
+		public int size() {	
 			// XXX IMPLEMENT ME XXX
 			return 0;
 		}
@@ -38,21 +59,29 @@ public class VSLogicalClockProtocol extends Protocol {
 	// --- Interface for LamportLockProtocol class ---
 	
 	public static int getMessageTime(Message m) {
-		// XXX IMPLEMENT ME XXX
-		return 0;
+		ClockHeader header = (ClockHeader) m.getHeader((short) 1501);
+		if(header != null)
+			return header.HeaderCounter;
+		else
+			return 0;
 	}
 	
 	// --- Protocol implementation ---
 	
 	@Override
 	public Object down(Event evt) {
-		// XXX IMPLEMENT ME XXX
+		switch (evt.getType()) {
+		case Event.MSG:
+			ClockHeader header =  
+			break;
+		}
 		return down_prot.down(evt); 
 	}
 	
 	@Override
 	public Object up(Event evt) {
 		// XXX IMPLEMENT ME XXX
+		
 		return up_prot.up(evt);
 	}
 	
